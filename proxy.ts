8687e96f40f9118d911ac438fb302d15ab3e78d4
stew.chan@gmail.com <stew.chan@gmail.com>
2026-05-20 19:57:07 -0400

    Use AuthKit proxy for Next 16

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
-import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
+import { authkitProxy } from "@workos-inc/authkit-nextjs";
 
-export default authkitMiddleware();
+export default authkitProxy();
 
 export const config = {
   matcher: ["/admin/:path*", "/hub/:path*"],
